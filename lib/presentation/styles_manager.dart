6ef5245fd39ba9127ee212247bde788e33ab16f9
Namesh Kushantha <44038404+techkush@users.noreply.github.com>
2022-01-03 10:14:45 +0530

    Lecture 8 - Creating Styles Manager

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_archi/presentation/font_manager.dart';
+
+TextStyle _getTextStyle(
+    double fontSize, String fontFamily, Color color, FontWeight fontWeight) {
+  return TextStyle(
+      fontSize: fontSize,
+      fontFamily: fontFamily,
+      color: color,
+      fontWeight: fontWeight);
+}
+
+// Regular Style
+TextStyle getRegularStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, color, FontWeightManager.regular);
+}
+
+// Light Text Style
+TextStyle getLightStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, color, FontWeightManager.light);
+}
+
+// Bold Text Style
+TextStyle getBoldStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, color, FontWeightManager.bold);
+}
+
+// Semi Bold Text Style
+TextStyle getSemiBoldStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, color, FontWeightManager.semiBold);
+}
+
+// Medium Text Style
+TextStyle getMediumStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, color, FontWeightManager.medium);
+}
