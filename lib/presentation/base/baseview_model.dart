cd149f4c510eb4b45d18530acd0ce74e50cd90d9
Namesh Kushantha <44038404+techkush@users.noreply.github.com>
2022-01-25 14:32:51 +0530

    Lecture 33 - Start MVVM Model

---
+++ lib/presentation/base/baseview_model.dart
@@ -0,0 +1,11 @@
+abstract class BaseViewModel {
+  // shared variables and functions that will be used through any view model.
+
+}
+
+abstract class BaseViewModelInputs {
+  void start(); // will be called while init. of view model
+  void dispose(); // will be called when view model dies.
+}
+
+abstract class BaseViewModelOutputs {}
