e9e4f502d7a291b06bea4a020b4eed7f060b3111
Namesh Kushantha <44038404+techkush@users.noreply.github.com>
2022-01-26 01:23:12 +0530

    Lecture 46 - Data Layer - Json Serialization

--- lib/data/responses.dart
+++ lib/data/responses.dart
@@ -1,7 +1,9 @@
 import 'package:json_annotation/json_annotation.dart';
 
+part 'responses.g.dart';
+
 @JsonSerializable()
-class BaseResponse{
+class BaseResponse {
   @JsonKey(name: "status")
   int? status;
   @JsonKey(name: "message")
@@ -9,29 +11,49 @@ class BaseResponse{
 }
 
 @JsonSerializable()
-class CustomerResponse{
+class CustomerResponse {
   @JsonKey(name: "id")
   String? id;
   @JsonKey(name: "name")
   String? name;
   @JsonKey(name: "numOfNotifications")
   int? numOfNotifications;
+
+  CustomerResponse(this.id, this.name, this.numOfNotifications);
+
+  factory CustomerResponse.fromJson(Map<String, dynamic> json) =>
+      _$CustomerResponseFromJson(json);
+  Map<String, dynamic> toJson() => _$CustomerResponseToJson(this);
 }
 
 @JsonSerializable()
-class ContactsResponse{
+class ContactsResponse {
   @JsonKey(name: "email")
   String? email;
   @JsonKey(name: "phone")
   String? phone;
   @JsonKey(name: "link")
   String? link;
+
+  ContactsResponse(this.email, this.phone, this.link);
+
+  factory ContactsResponse.fromJson(Map<String, dynamic> json) =>
+      _$ContactsResponseFromJson(json);
+  Map<String, dynamic> toJson() => _$ContactsResponseToJson(this);
 }
 
 @JsonSerializable()
-class AuthenticationResponse extends BaseResponse{
+class AuthenticationResponse extends BaseResponse {
   @JsonKey(name: "customer")
   CustomerResponse? customer;
   @JsonKey(name: "contacts")
   ContactsResponse? contacts;
+
+  AuthenticationResponse(this.customer, this.contacts);
+
+  factory AuthenticationResponse.fromJson(Map<String, dynamic> json) =>
+      _$AuthenticationResponseFromJson(json);
+  Map<String, dynamic> toJson() => _$AuthenticationResponseToJson(this);
 }
+
+// flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
